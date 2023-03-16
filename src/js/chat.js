d2a6b7226bf269153909ff33ad72c66e6991a8b8
John Doe <leehy0782@naver.com>
2023-03-16 18:11:14 +0900

    Feat: 유저 답변 인코딩 함수 및 api로 보낼 데이터 생성

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,5 +1,5 @@
-const $chatRoom = document.querySelector('.chatroom');
-const $chatBtn = document.querySelector('.cont-chat .btn-open');
+const $chatRoom = document.querySelector(".chatroom");
+const $chatBtn = document.querySelector(".cont-chat .btn-open");
 const $chatInput = document.querySelector(".inp-chat input");
 const $sendForm = document.querySelector(".inp-chat");
 
@@ -29,17 +29,17 @@ const encode = (text) => {
 };
 
 // 버튼 누르면 채팅창 활성화시키는 함수
-$chatBtn.addEventListener('click',()=>{
-  $chatRoom.classList.toggle('open')
-})
+$chatBtn.addEventListener("click", () => {
+  $chatRoom.classList.toggle("open");
+});
 
 // 유저 질문 받아오는 함수
 // JSON ESCAPE 적용해야함
 $chatInput.addEventListener("change", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   if (question != e.target.value) {
     question = e.target.value;
-  };
+  }
 });
 
 // 유저의 질문을 담은 객체를 data arr에 push
@@ -57,33 +57,30 @@ $sendForm.addEventListener("click", (e) => {
     sendQuestion(question);
   }
 
-  console.log(data)
-  // apiTest(config);
+  console.log(data);
+  apiTest(config);
 });
 
 // API 통신 관련 함수
-// let config = {
-//   method: "post",
-//   maxBodyLength: Infinity,
-//   url: "http://3.37.150.96/chat",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Credentials": true,
-//   },
-//   withCredentials: true,
-//   data: JSON.stringify(data),
-// };
+let config = {
+  method: "post",
+  maxBodyLength: Infinity,
+  url: "http://3.37.150.96/chat",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  data: JSON.stringify(data),
+};
 
-// const apiTest = (config) => {
-//   console.log("함수가 작동을 하기는 해");
-//   axios(config)
-//     .then(function (response) {
-//       console.log(111111111111);
-//       console.log(JSON.stringify(response.data));
-//     })
-//     .catch(function (err) {
-//       console.log(2222222222222);
-//       console.log(err);
-//     });
-// };
+const apiTest = (config) => {
+  console.log("함수가 작동을 하기는 해");
+  axios(config)
+    .then(function (response) {
+      console.log(111111111111);
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (err) {
+      console.log(2222222222222);
+      console.log(err);
+    });
+};
