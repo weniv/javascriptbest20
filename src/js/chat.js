63355bae802c3c6b66f5d03a322752ba14e1b00f
John Doe <leehy0782@naver.com>
2023-03-17 11:41:59 +0900

    Feat: 인코딩 함수 및 data에 질문 푸시 함수 수정

--- src/js/chat.js
+++ src/js/chat.js
@@ -7,6 +7,7 @@ const $sendForm = document.querySelector(".inp-chat");
 let question;
 
 // 질문과 답변을 저장하는 객체
+// data 내부에서 이스케이프 문자열 인식안됨 \u이 아닌 \\u로 인식되는 문제가 발생함
 let data = [
   {
     role: "system",
@@ -17,13 +18,14 @@ let data = [
 // JSON ESCAPE 인코딩 함수
 const encode = (text) => {
   let str = "";
-  for (let i = 0; i < text.length; i++) {
-    if (String(text.charCodeAt(i)).length >= 3) {
-      str += String(`\\u` + text.charCodeAt(i).toString(16));
+  for (let i = 0; i < text?.length; i++) {
+    if (String(text.charCodeAt(i)).length > 3) {
+      str += String("\\u"  + text.charCodeAt(i).toString(16));
     } else {
       str += String(text[i]);
     }
   }
+  console.log("str");
   console.log(str);
   return str;
 };
@@ -34,7 +36,6 @@ $chatBtn.addEventListener("click", () => {
 });
 
 // 유저 질문 받아오는 함수
-// JSON ESCAPE 적용해야함
 $chatInput.addEventListener("change", (e) => {
   e.preventDefault();
   if (question != e.target.value) {
@@ -45,39 +46,42 @@ $chatInput.addEventListener("change", (e) => {
 // 유저의 질문을 담은 객체를 data arr에 push
 const sendQuestion = (question) => {
   data.push({
-    role: "user",
-    content: encode(question),
-  });
+      role: "user",
+      content: encode(question),  
+    });
+
+  // console.log("encode(question)");
+  console.log(encode(question));
+  console.log(data);
+  // console.log(typeof data);
 };
 
 $sendForm.addEventListener("click", (e) => {
   e.preventDefault();
   // 이전 질문과 동일한 질문일 경우 data arr에 push 하지않음
-  if (question && data[data.length - 1].content !== question) {
+  if (data[data.length - 1].content !== encode(question)) {
     sendQuestion(question);
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://3.37.150.96/chat",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // data: JSON.stringify(data),
+      data: data,
+    };
+    apiTest(config);
   }
-
-  console.log(data);
-  apiTest(config);
 });
 
 // API 통신 관련 함수
-let config = {
-  method: "post",
-  maxBodyLength: Infinity,
-  url: "http://3.37.150.96/chat",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  data: JSON.stringify(data),
-};
-
-const apiTest = (config) => {
-  console.log("함수가 작동을 하기는 해");
-  axios(config)
+const apiTest = async(config) => {
+  let result = await axios(config)
     .then(function (response) {
       console.log(111111111111);
-      console.log(JSON.stringify(response.data));
+      const result = response.data.choices[0].message.content;
+      console.log(JSON.stringify(result));
     })
     .catch(function (err) {
       console.log(2222222222222);
