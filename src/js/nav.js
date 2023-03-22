8962f3e032e6043d44ae57d7081762376180062f
Hye-yeong, Lee <leehy0782@gmail.com>
2023-03-22 10:46:21 +0900

    Revert "Feat: 인공지능 채팅 기능 추가"

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,79 +1,45 @@
 const $container = document.querySelector(".container");
-const $main = $container.querySelector('.contents');
 const $btnFold = document.querySelector(".btn-fold");
-const $nav = $main.querySelector(".question-nav");
+const $nav = document.querySelector("nav");
 const $codeMirror = document.querySelector(".CodeMirror");
-const $btnMenu = $container.querySelector('.hamburger-btn');
-const $icon = $btnMenu.querySelector("img");
-const $menuContainer = $container.querySelector('.menu-nav');
 let isMobile = null;
 
-const handleCloseQuestions = () => {
+const handleClose = () => {
   $container.classList.remove("menu-on");
   $btnFold.innerText = "메뉴 펼치기";
-  $codeMirror.classList.remove("menu-on-CodeMirror");
-  $codeMirror.classList.add("menu-off-CodeMirror");
+  $codeMirror.classList.remove("menu-on-CodeMirror")
+  $codeMirror.classList.add("menu-off-CodeMirror")
 }
-
-const handleOpenQuestions = () => {
-  if($menuContainer.classList.contains('is-active')){
-    handleCloseMenu();
-  }
-
+const handleOpen = () => {
   $container.classList.add("menu-on");
   $btnFold.innerText = "메뉴 접기";
-  $codeMirror.classList.add("menu-on-CodeMirror");
-  $codeMirror.classList.remove("menu-off-CodeMirror");
+  $codeMirror.classList.add("menu-on-CodeMirror")
+  $codeMirror.classList.remove("menu-off-CodeMirror")
 }
-
-const handleToggleQuestions = () => {
-  if ($container.classList.contains("menu-on")) {
-    handleCloseQuestions();
-  } else {
-    handleOpenQuestions();
-  }
-};
 
 const handleToggleMenu = () => {
-  let isActive = $menuContainer.classList.contains('is-active');
-  
-  $menuContainer.classList.toggle('is-active');
-  $icon.src = isActive ? 'src/img/hamburger-btn.webp' : 'src/img/close.webp';
-  $icon.alt = isActive ? '메뉴 열기' : '메뉴 닫기';
-  
-  if(isActive) {
-    $icon.classList.remove('close');
+  if ($container.classList.contains("menu-on")) {
+    handleClose();
   } else {
-    $icon.classList.add('close');
-  }
 
-  if($menuContainer.classList.contains('is-active')){
-    handleCloseQuestions();
+    handleOpen();
   }
 };
 
-const handleCloseMenu = () => {
-  $menuContainer.classList.remove('is-active');
-  $icon.classList.remove('close');
-  $icon.src = 'src/img/hamburger-btn.webp';
-}
+
 
 const checkMobile = () => {
   const winWidth = window.innerWidth;
 
-  if (winWidth > 1024) {
+  if(winWidth > 1024){
     isMobile = false;
-    $menuContainer.classList.remove('is-active');
-  } else if (winWidth >= 820) {
+  }else{
+    handleClose();
     isMobile = true;
-    handleCloseMenu();
-  } else {
-    isMobile = true;
-    handleCloseQuestions();
   }
 }
 
-const handleResizeWidth = () =>{
+const handleResizeWidth =() =>{
   let timer = null;
   clearTimeout(timer);
   timer = setTimeout(function(){
@@ -81,23 +47,14 @@ const handleResizeWidth = () =>{
   },300)
 }
 
-const handleCloseMobileQuestions = (e) =>{
+const handleCloseMobileMenu =(e) =>{
   const check = Boolean(e.target.closest('nav')) || Boolean(e.target.closest('header'));
   if(isMobile && $container.classList.contains("menu-on") && !check){
-    handleCloseQuestions();
-  }
-}
-
-const handleClickOutside = (e) => {
-  if($main.contains(e.target)){
-    handleCloseMenu();
+    handleClose();
   }
 }
 
 checkMobile();
-$btnFold.addEventListener("click", handleToggleQuestions);
-$container.addEventListener("click", handleCloseMobileQuestions);
+$btnFold.addEventListener("click", handleToggleMenu);
+$container.addEventListener("click", handleCloseMobileMenu);
 window.addEventListener("resize", handleResizeWidth);
-
-$btnMenu.addEventListener('click', handleToggleMenu);
-$main.addEventListener('click', handleClickOutside);
